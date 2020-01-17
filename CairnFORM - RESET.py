6c7f801e6401a6e82dd767279e4d593ba18c36c6
maximedaniel <maxaxeldaniel@gmail.com>
2020-01-17 10:46:56 +0100

    Adding test scripts

---
+++ CairnFORM - RESET.py
@@ -0,0 +1,6 @@
+from random import *
+import paho.mqtt.client as mqtt
+from driver.StackController import StackController
+from driver.Transition import Transition
+
+stack = StackController()
---
+++ test/light-raspberrypi.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+from serial import *
+import time
+import struct
+
+
+class TestLight:
+    def __init__(self):
+        self.arduino = Serial(port="/dev/ttyACM0", baudrate=9600, timeout=10, writeTimeout=10)
+        time.sleep(2)
+        for address in range(12):
+            self.set(address,200,200,200)
+            time.sleep(1)
+            
+        for address in range(12):
+            self.set(address,0,0,0)
+            time.sleep(1)
+        
+    def set(self, address, r, g, b):
+            if self.arduino.isOpen():
+                self.arduino.flush() 
+                package = [address, int(r), int(g), int(b)]
+                print('writing ', package)
+                for i in package:
+                    self.arduino.write(struct.pack('B', i))
+            else:
+                print("arduino is closed !")
+
+
+TestLight()
