494721d9b35055e14dfd67b51c972c99cc0eee26
maximedaniel <maxaxeldaniel@gmail.com>
2020-01-22 12:05:54 +0100

    Fix

--- trash/CairnFORM-4R.py
+++ trash/CairnFORM-4R.py
@@ -5,6 +5,7 @@ import json
 from pprint import pprint
 
 stack = StackController()
+# stack.push([address<0:STACK_SIZE>,  R<0:255>,    G<0:255>,  B<0:255>, POSITION<0:200>, DELAY<0:Inf>, DURATIONDELAY<0:Inf>, TRANSITION<0:Inf>])
 stack.push([0,  20,    20,  20, 100, 0, 2, 'EASE_IN_OUT_QUINT'])
 stack.push([1,  20,    20,  20, 100, 0, 2, 'EASE_IN_OUT_QUINT'])
 stack.push([2,  20,    20,  20, 100, 0, 2, 'EASE_IN_OUT_QUINT'])
